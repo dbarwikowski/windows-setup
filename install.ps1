d8ece072da96fa5ff266b10599d34449aad32a20
Daniel Barwikowski <d.barwikowski@gmail.com>
2019-05-28 15:36:18 +0200

    Create install.ps1

---
+++ install.ps1
@@ -0,0 +1,4 @@
+if (!(Get-Command choco -errorAction SilentlyContinue))
+{
+    Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+}
